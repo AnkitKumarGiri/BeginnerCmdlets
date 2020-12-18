b3d134e79f43493ff537d63270b13b0526837dba
Ankit Giri <angiri@microsoft.com>
2020-12-18 07:10:47 +0530

    CmdletClass/OverridableMethods

--- BeginnerCmdlets/GetRandomName.cs
+++ BeginnerCmdlets/GetRandomName.cs
@@ -2,18 +2,35 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Management.Automation;
+using System.Threading;
 
 namespace BeginnerCmdlets
 {
     [Cmdlet(VerbsCommon.Get, "RandomName")]
     public class GetRandomName : Cmdlet
     {
-        [Parameter(Position = 1, Mandatory = true)]
+        [Parameter(Position = 1, Mandatory = true, ValueFromPipeline = true)]
         public string Name { get; set; }
 
         protected override void ProcessRecord()
         {
-            Console.WriteLine("{0} (Len {1})", Name, Name.Length);
+            Console.WriteLine("In ProcessRecord: {0} (Len {1})", Name, Name.Length);
+            Thread.Sleep(2000);
+        }
+
+        protected override void BeginProcessing()
+        {
+            Console.WriteLine("In BeginProcessing");
+        }
+
+        protected override void EndProcessing()
+        {
+            Console.WriteLine("In EndProcessing");
+        }
+
+        protected override void StopProcessing()
+        {
+            Console.WriteLine("In StopProcessing");
         }
     }
 }
