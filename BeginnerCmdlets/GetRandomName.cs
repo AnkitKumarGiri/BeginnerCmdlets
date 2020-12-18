17881e06a3acf3ed8bcb6d225d9bbd416bbf21cb
Ankit Giri <angiri@microsoft.com>
2020-12-18 06:38:49 +0530

    Add project files.

---
+++ BeginnerCmdlets/GetRandomName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management.Automation;
+
+namespace BeginnerCmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "RandomName")]
+    public class GetRandomName : Cmdlet
+    {
+        [Parameter]
+        public string Name { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            Console.WriteLine("{0} (Len {1})", Name, Name.Length);
+        }
+    }
+}
